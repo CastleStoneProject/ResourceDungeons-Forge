e652f477f0127e3c640fcb4344a405b90bd0df22
thekarura <karura2310@yahoo.co.jp>
2018-07-06 11:14:20 +0900

    説明文の追加とカラーリングに対応

--- src/main/java/net/tkarura/resourcedungeons/forge/ForgeReloadCommand.java
+++ src/main/java/net/tkarura/resourcedungeons/forge/ForgeReloadCommand.java
@@ -4,17 +4,19 @@ import net.tkarura.resourcedungeons.core.command.DungeonCommand;
 import net.tkarura.resourcedungeons.core.command.DungeonCommandSender;
 import net.tkarura.resourcedungeons.core.exception.DungeonCommandException;
 
+import static net.tkarura.resourcedungeons.core.ResourceDungeons.PREFIX_MES;
+
 public class ForgeReloadCommand extends DungeonCommand {
 
     public ForgeReloadCommand() {
         super("reload");
-        this.description = "Setting Reload.";
+        this.description = "設定の再読込を行います。";
     }
 
     @Override
     public void execute(DungeonCommandSender sender) throws DungeonCommandException {
-        sender.sendMessage("Reload Start.");
+        sender.sendMessage(PREFIX_MES + " &6リロードをします。");
         ResourceDungeonsForge.getInstance().init();
-        sender.sendMessage("Reload Complete.");
+        sender.sendMessage(PREFIX_MES + " &aリロードが完了しました。");
     }
 }
